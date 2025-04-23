cd5ef1929b67f256459d90e329357f410764b69d
ErwanMettouchi <erwanmettouchi@gmail.com>
2025-04-23 18:34:51 +0200

    feat: add review model with sequelize

---
+++ app/models/review.model.js
@@ -0,0 +1,19 @@
+import { DataTypes, Sequelize } from "sequelize";
+import { sequelize } from "../data/client.js";
+
+export class Review extends Sequelize.Model {}
+Review.init(
+  {
+    grade: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    sequelize,
+    tableName: "review",
+  }
+);
