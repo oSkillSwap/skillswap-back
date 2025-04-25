6fce72bb383696bd82dae99d0a5331a9a7f043b5
GreyGiver <greygivers@gmail.com>
2025-04-25 09:25:06 +0200

    feat: add middleware validation params URL

--- app/routers/router.js
+++ app/routers/router.js
@@ -6,17 +6,33 @@ import { userController } from "../controllers/user.controller.js";
 import { controllerwrapper } from "../middlewares/controllerwrapper.js";
 import { validate } from "../middlewares/validates.js";
 import { loginSchema, registerSchema } from "../schemas/user.schema.js";
+import { validateParams } from "../middlewares/validateParams.js";
 
 export const router = Router();
 
 router.get("/users", controllerwrapper(userController.getUsers));
-router.post('/register', validate(registerSchema), controllerwrapper(userController.register));
-router.post('/login', validate(loginSchema), controllerwrapper(userController.login));
+router.post(
+	"/register",
+	validate(registerSchema),
+	controllerwrapper(userController.register),
+);
+router.post(
+	"/login",
+	validate(loginSchema),
+	controllerwrapper(userController.login),
+);
 
 router.get("/reviews", controllerwrapper(reviewController.getReviews));
 router.get("/categories", controllerwrapper(categoryontroller.getCategories));
-router.get("/messages/:id", controllerwrapper(messageController.getMessages));
+router.get(
+	"/messages/:id",
+	validateParams("id"),
+	controllerwrapper(messageController.getMessages),
+);
 
 router
-  .route("/messages/:me/:userId")
-  .get(controllerwrapper(messageController.getConversation));
+	.route("/messages/:me/:userId")
+	.get(
+		validateParams("me", "userId"),
+		controllerwrapper(messageController.getConversation),
+	);
--- app/schemas/user.schema.js
+++ app/schemas/user.schema.js
@@ -6,10 +6,10 @@ export const registerSchema = z.object({
     .min(1, "Le nom d'utilisateur est requis")
     .max(16, "Maximum 16 caractères")
     .regex(/^[a-zA-Z0-9-]+$/, "Le nom d'utilisateur ne peut contenir que des lettres, des chiffres ou des tirets"),
-  lastName: z.string().optional(),
-  firstName: z.string().optional(),
-  email: z.string().email("Adresse e-mail invalide"),
-  password: z.string()
+    lastName: z.string().optional(),
+    firstName: z.string().optional(),
+    email: z.string().email("Adresse e-mail invalide"),
+    password: z.string()
     .min(8, 'Le mot de passe doit faire au moins 8 caractères')
     .regex(/[a-z]/, 'Le mot de passe doit contenir au moins une lettre minuscule')
     .regex(/[A-Z]/, 'Le mot de passe doit contenir au moins une lettre majuscule')
