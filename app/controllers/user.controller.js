9d75bb0afac037493a524498e8d4a7ffe80f36c5
ErwanMettouchi <erwanmettouchi@gmail.com>
2025-05-15 09:57:27 +0200

    test refresh token

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -90,7 +90,7 @@ export const userController = {
     res
       .cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: false, // true if using https
+        secure: true, // true if using https
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours
       })
